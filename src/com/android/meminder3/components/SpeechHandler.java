88cb5afd9f8d6fa5b0e880c1bfd073163f2f1658
DanaA <dana.akerman@gmail.com>
2014-06-15 10:37:04 +0300

    fixed tts issue

--- src/com/android/meminder3/components/SpeechHandler.java
+++ src/com/android/meminder3/components/SpeechHandler.java
@@ -14,14 +14,12 @@ import android.util.Log;
 
 public class SpeechHandler 
 {
+	// singleton
 	private static SpeechHandler instance;
+	
+	// data members
 	private String _toSpeak;
 	private TextToSpeech _tts; // you need to stop and shutdown somehow.
-	
-	
-	private SpeechHandler()
-	{
-	}
 	
 	
 	public static SpeechHandler getInstance()
@@ -32,6 +30,8 @@ public class SpeechHandler
 		}
 		return instance;
 	}
+	
+	//----------------------------------------------------------------------------
 	
 	public void initTTS(Context context)
 	{
@@ -53,6 +53,8 @@ public class SpeechHandler
 		});
 	}
 	
+	//----------------------------------------------------------------------------
+	
 	public void shutDownTTS()
 	{
 		if(_tts != null)
@@ -62,64 +64,52 @@ public class SpeechHandler
 		}
 	}
 	
+	//----------------------------------------------------------------------------
 	
 	public void reminderToSpeech(ArrayList<ActivityData> reminder)
 	{
-		if(_tts != null)
+		if(reminder.isEmpty())
+			_toSpeak = "hello! no reminder today.";
+		else
 		{
-			if(reminder.isEmpty())
+			String text = "";
+
+			for(int i = 0; i < reminder.size(); i++)
 			{
-				//_tts.speak("hello! no reminder today." , TextToSpeech.QUEUE_FLUSH, null);
-				_toSpeak = "hello! no reminder today.";
-			}
-			else
-			{
-				String text = "";
-	
-				for(int i = 0; i < reminder.size(); i++)
+				ActivityData curr = reminder.get(i);
+
+				String title = curr.getTitle();
+				String prepWord =  curr.getPrepWord();
+				String subject = curr.getSubject();
+				int numOfDays = -1;
+
+				if(!curr.getType().equals("task")) // if this is NOT a task
+					numOfDays = curr.getNumDays();
+
+				String time = "";
+
+				if(curr.hasTime())
+					time += "at " + utils.hourAndMinutesToSpeechFormatString(curr.getHour(), curr.getMinutes());
+
+				if(numOfDays >= 0)
 				{
-					ActivityData curr = reminder.get(i);
-					
-					String title = curr.getTitle();
-					String prepWord =  curr.getPrepWord();
-					String subject = curr.getSubject();
-					int numOfDays = -1;
-					
-					if(!curr.getType().equals("task")) // if this is NOT a task
-					{
-						numOfDays = curr.getNumDays();
-					}
-					
-					String time = "";
-					
-					if(curr.hasTime())
-					{
-						time += "at " + utils.hourAndMinutesToSpeechFormatString(curr.getHour(), curr.getMinutes());
-					}
-					
-					if(numOfDays >= 0)
-					{
-						if(numOfDays == 0)
-							text += "today: ";
-						else if(numOfDays == 1)
-							text += "tomorrow: ";
-						else if(numOfDays == 7)
-							text += "in one week: ";
-						else if(numOfDays == 14)
-							text +="in 2 weeks: ";
-						else if(numOfDays == 21)
-							text += "in 3 weeks: ";
-						else
-							text += "in " + numOfDays + " days: ";
-					}
-					
-					text += title + " " + prepWord + " " + subject + " " + time + "; ";
-					
-					
+					if(numOfDays == 0)
+						text += "today: ";
+					else if(numOfDays == 1)
+						text += "tomorrow: ";
+					else if(numOfDays == 7)
+						text += "in one week: ";
+					else if(numOfDays == 14)
+						text +="in 2 weeks: ";
+					else if(numOfDays == 21)
+						text += "in 3 weeks: ";
+					else
+						text += "in " + numOfDays + " days: ";
 				}
-				//_tts.speak("The reminder is: " + text , TextToSpeech.QUEUE_FLUSH, null);
-				_toSpeak = text;
+
+				text += title + " " + prepWord + " " + subject + " " + time + "; ";
 			}
+			_toSpeak = text;
 		}
 	}
 }
